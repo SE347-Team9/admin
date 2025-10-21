57a385337062d06a45a208c60d93467efbb823eb
nghiagenz <email_cua_nghiagenz@gmail.com>
2025-10-21 18:54:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './routes/auth/Login.tsx'
 import Register from './routes/auth/Register.tsx'
+import Reports from './pages/Report/Reports.tsx'
+import AddReport from './pages/Report/AddReport.tsx'
+import ViewReport from './pages/Report/ViewReport.tsx'
+import MainLayout from './components/layout/MainLayout.tsx'
 
 function App() {
   return (
@@ -8,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/reports" element={<MainLayout><Reports /></MainLayout>} />
+        <Route path="/add-report" element={<MainLayout><AddReport /></MainLayout>} />
+        <Route path="/view-report/:reportId" element={<MainLayout><ViewReport /></MainLayout>} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
